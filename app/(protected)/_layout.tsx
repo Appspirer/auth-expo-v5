70e5600d86d33e970f96a4edc7ac17680b193848
Huỳnh Quang Huy <quanghuy9742@gmail.com>
2025-07-28 15:01:12 +0700

    Redirect to the login screen if there is no authorization

--- app/(protected)/_layout.tsx
+++ app/(protected)/_layout.tsx
@@ -1,7 +1,14 @@
-import { Stack } from "expo-router";
-import React from "react";
+import { Redirect, Stack, useRouter } from "expo-router";
+import React, { useState } from "react";
 
 export default function ProtectedLayout() {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const router = useRouter();
+
+  if (!isLoggedIn) {
+    return <Redirect href="/login" />;
+  }
+
   return (
     <Stack>
       <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
