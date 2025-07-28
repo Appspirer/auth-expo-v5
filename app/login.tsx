b7fa4c7f4473f10e2e2f9f4c755801ef3bc6549f
Huỳnh Quang Huy <quanghuy9742@gmail.com>
2025-07-28 18:24:25 +0700

    Add login button

--- app/login.tsx
+++ app/login.tsx
@@ -1,10 +1,14 @@
-import React from "react";
-import { Text, View } from "react-native";
+import { AuthContext } from "@/contexts/authContext";
+import React, { useContext } from "react";
+import { Button, Text, View } from "react-native";
 
 export default function LoginScreen() {
+  const { logIn } = useContext(AuthContext);
+
   return (
     <View>
       <Text>LoginScreen</Text>
+      <Button title="Login" onPress={() => logIn()} />
     </View>
   );
 }
