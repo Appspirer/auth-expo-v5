2066292b569a9f12f8ef9d6555644c4cef4b23b2
Huỳnh Quang Huy <quanghuy9742@gmail.com>
2025-07-28 18:24:09 +0700

    Create login function in AuthContext

--- contexts/authContext.tsx
+++ contexts/authContext.tsx
@@ -1,14 +1,21 @@
+import { router } from "expo-router";
 import { createContext, PropsWithChildren, useState } from "react";
 
 export const AuthContext = createContext({
   isLoggedIn: false,
+  logIn: () => {},
 });
 
 export default function AuthProvider({ children }: PropsWithChildren) {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
 
+  const logIn = () => {
+    setIsLoggedIn(true);
+    router.replace("/");
+  };
+
   return (
-    <AuthContext.Provider value={{ isLoggedIn }}>
+    <AuthContext.Provider value={{ isLoggedIn, logIn }}>
       {children}
     </AuthContext.Provider>
   );
