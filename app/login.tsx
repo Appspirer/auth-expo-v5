78c78593ab63b29f7fd5779caf5ea4785a421968
Huỳnh Quang Huy <quanghuy9742@gmail.com>
2025-07-28 15:01:47 +0700

    Fix bug text

--- app/login.tsx
+++ app/login.tsx
@@ -1,6 +1,10 @@
 import React from "react";
-import { View } from "react-native";
+import { Text, View } from "react-native";
 
 export default function LoginScreen() {
-  return <View>LoginScreen</View>;
+  return (
+    <View>
+      <Text>LoginScreen</Text>
+    </View>
+  );
 }
