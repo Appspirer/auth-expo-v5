b79977c3d9d515f76f0b9d0c30488f0e5ae7cf7e
Huỳnh Quang Huy <quanghuy9742@gmail.com>
2025-07-29 16:29:06 +0700

    Add get user info function

--- utils/secureStore.ts
+++ utils/secureStore.ts
@@ -3,6 +3,11 @@ import * as SecureStore from "expo-secure-store";
 
 const KEY_USER_INFO = "user_info";
 
+export const getUserInfo = async () => {
+  const userInfo = await SecureStore.getItemAsync(KEY_USER_INFO);
+  return userInfo;
+};
+
 export const setUserInfo = async (userInfo: UserInfo) => {
   await SecureStore.setItemAsync(KEY_USER_INFO, JSON.stringify(userInfo));
 };
