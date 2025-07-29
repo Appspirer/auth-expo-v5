9c49145c4d7e6f40d9986d6c93fc18ab2c3b46b1
Huỳnh Quang Huy <quanghuy9742@gmail.com>
2025-07-29 13:03:23 +0700

    Handle loading in login screen

--- app/login.tsx
+++ app/login.tsx
@@ -1,14 +1,32 @@
 import { AuthContext } from "@/contexts/authContext";
-import React, { useContext } from "react";
-import { Button, Text, View } from "react-native";
+import React, { useContext, useState } from "react";
+import { ActivityIndicator, Button, Text, View } from "react-native";
 
 export default function LoginScreen() {
+  const [loading, setLoading] = useState(false);
   const { logIn } = useContext(AuthContext);
 
+  const login = async () => {
+    try {
+      setLoading(true);
+      await logIn();
+    } catch (error) {
+      console.log(JSON.stringify(error));
+    } finally {
+      setLoading(false);
+    }
+  };
+
   return (
-    <View>
-      <Text>LoginScreen</Text>
-      <Button title="Login" onPress={() => logIn()} />
+    <View style={{ flex: 1 }}>
+      {loading ? (
+        <ActivityIndicator size="large" color="red" />
+      ) : (
+        <>
+          <Text>LoginScreen</Text>
+          <Button title="Login" onPress={login} />
+        </>
+      )}
     </View>
   );
 }
