d0d5e339a6cb37aa3145bfbaaa309c17de677a50
Huỳnh Quang Huy <quanghuy9742@gmail.com>
2025-07-29 13:08:23 +0700

    Handle loading in logout screen

--- app/(protected)/(tabs)/index.tsx
+++ app/(protected)/(tabs)/index.tsx
@@ -1,14 +1,32 @@
 import { AuthContext } from "@/contexts/authContext";
-import { useContext } from "react";
-import { Button, SafeAreaView, Text } from "react-native";
+import { useContext, useState } from "react";
+import { ActivityIndicator, Button, SafeAreaView, Text } from "react-native";
 
 export default function HomeScreen() {
+  const [loading, setLoading] = useState(false);
   const { logOut } = useContext(AuthContext);
+
+  const logout = async () => {
+    try {
+      setLoading(true);
+      await logOut();
+    } catch (error) {
+      console.log(error);
+    } finally {
+      setLoading(false);
+    }
+  };
 
   return (
     <SafeAreaView>
-      <Text>Home screen</Text>
-      <Button title="Logout" onPress={() => logOut()} />
+      {loading ? (
+        <ActivityIndicator size={"large"} color={"red"} />
+      ) : (
+        <>
+          <Text>Home screen</Text>
+          <Button title="Logout" onPress={logout} />
+        </>
+      )}
     </SafeAreaView>
   );
 }
